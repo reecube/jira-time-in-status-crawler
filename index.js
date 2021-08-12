a4c7722a24a9555b39f68fa38f77fa8bcebfd9a7
Yves Riedener <git@reecube.com>
2021-08-12 17:16:14 +0200

    .env support

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
+
+const envValidator = {
+  JIRA_DOMAIN: 'string',
+  JIRA_VERSION: 'string',
+  JIRA_USERNAME: 'string',
+  JIRA_PASSWORD: 'string',
+};
+
+for (const entry of Object.entries(envValidator)) {
+  const [key, type] = entry;
+
+  if (typeof process.env[key] !== type)
+    throw new Error(`Invalid type '${typeof process.env[key]}' for .env key '${key}'!`);
+}
+
 console.log('YAY!');
