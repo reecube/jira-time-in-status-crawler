ed8ec6481f293d78016d7e30f9f85f8592c462a7
Yves Riedener <git@reecube.com>
2021-08-13 17:12:26 +0200

    implement crawl and build => add csv export

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ require('dotenv').config();
 require('./lib/validator');
 
 const context = require('./lib/jira');
+const crawl = require("./lib/crawl");
 
-const crawl = require('./lib/crawl');
+const ACTION_CRAWL = 'crawl';
+const ACTION_BUILD = 'build';
 
-(async ()=> {
-  await crawl(context);
+const ACTIONS = [
+  ACTION_CRAWL,
+  ACTION_BUILD,
+];
+
+(async () => {
+  const selectedAction = (process.argv[2] || '').toLowerCase();
+
+  if (!ACTIONS.includes(selectedAction))
+    return console.error(`Invalid action '${selectedAction}'! Please select one of: ${ACTIONS.join(', ')}`);
+
+
+  switch (selectedAction) {
+    case ACTION_CRAWL:
+      const crawl = require('./lib/crawl');
+
+      return await crawl(context);
+    case ACTION_BUILD:
+      const build = require('./lib/build');
+
+      return await build(context);
+    default:
+      throw new Error(`Unexpected action '${selectedAction}'!`);
+  }
 })();
